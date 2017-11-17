9209b825177eef457ea9826337b9d140b06a89b1
Kenny Ward <hello@kennyward.co.uk>
2017-11-17 01:47:39 +0000

    Fixed description

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The template for displaying portfolio archive pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
